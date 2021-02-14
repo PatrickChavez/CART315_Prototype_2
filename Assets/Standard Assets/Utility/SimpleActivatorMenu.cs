cd5a3508f417bc3612f3f5029ad074210c3ecfa3
Patrick Chavez <patrickchavez64@yahoo.ca>
2021-02-14 14:01:45 -0500

    Fixed the SimpleActivatorMenu.cs
    Solution code from Ryiah
    https://forum.unity.com/threads/standard-assets-2018-let-us-know-what-you-think.516595/page-4#post-5458527

--- Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
+++ Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
@@ -1,14 +1,16 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
+//Corrected code from Ryiah
 
-#pragma warning disable 618
 namespace UnityStandardAssets.Utility
 {
     public class SimpleActivatorMenu : MonoBehaviour
     {
         // An incredibly simple menu which, when given references
         // to gameobjects in the scene
-        public GUIText camSwitchButton;
+        //public GUIText camSwitchButton;
+        public Text camSwitchButton;
         public GameObject[] objects;
 
 
