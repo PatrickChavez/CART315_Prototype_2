a5300205552d127d3717de6456ee142b5152f0cb
Patrick Chavez <patrickchavez64@yahoo.ca>
2021-02-15 20:33:34 -0500

    Added a CameraFollow script
    Code by Brackeys
    
    https://www.youtube.com/watch?v=MFQhpwc6cKE

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Code by Brackeys
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(target);
+    }
+}
